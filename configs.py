62d07b63911cd02ae287486f764ccb57dfc4792a
Team MM <shamil3018@gmail.com>
2023-11-24 23:04:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,8 +4,8 @@ class Config:
     API_ID = int(getenv("API_ID", ""))
     API_HASH = getenv("API_HASH", "")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
-    FSUB = getenv("FSUB", "MovieMalonie")
-    CHID = int(getenv("CHID", "-1002098260161"))
+    FSUB = getenv("FSUB", "https://t.me/+4Zpc9QN0XLcwOGU0")
+    CHID = int(getenv("CHID", "-1001083210237"))
     SUDO = list(map(int, getenv("SUDO").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
