1736dd926c99fa75fe2d5037f4380f70032da8ec
Shamil Habeeb <shamilhabeeb5@gmail.com>
2024-09-01 15:59:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -127,8 +127,8 @@ async def chk(_, cb : CallbackQuery):
 
 @app.on_message(filters.command("users") & filters.user(cfg.SUDO))
 async def dbtool(_, m : Message):
-    xx = 1000000 + all_users()
-    x = 10000 + all_groups()
+    xx = all_users()
+    x = all_groups()
     tot = int(xx + x)
     await m.reply_text(text=f"""
 🍀 Chats Stats 🍀
